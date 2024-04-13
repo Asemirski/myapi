69055ee8926ecaeea39a6ac47165e61ee2d9bba9
Aliaksei Semirski <aliaksei_semiski@epam.com>
2024-04-13 11:58:45 +0200

    common api

--- main.py
+++ main.py
@@ -13,4 +13,4 @@ async def ask_question(question: Question):
     return {"reply": processor.processQuestion(userPrompt=question)}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8080)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
